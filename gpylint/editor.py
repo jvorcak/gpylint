d62a5604f3481592c880002d418af7e630fd6d85
Jan Vorcak <vorcak@mail.muni.cz>
2012-04-03 03:21:56 +0200

    After click on error tag, error message is printed to the console

--- gpylint/editor.py
+++ gpylint/editor.py
@@ -1,6 +1,5 @@
 from gi.repository import Gtk, GtkSource, Vte, GLib
 
-
 class Editor(object):
 
     """
@@ -32,7 +31,7 @@ class Editor(object):
             'I': self.info_line,
             'R': self.refactor_line,
             'F': self.fatal_line
-        }[sigle](line, msg)
+            }[sigle](line-1, msg)
 
 class VimEditor(Editor):
 
@@ -45,6 +44,24 @@ class VimEditor(Editor):
         self.component.fork_command_full(Vte.PtyFlags.DEFAULT, None, ['vim',\
                 self.filepath], [], GLib.SpawnFlags.SEARCH_PATH, None, None)
         self.component.show()
+
+    def error_line(self, line_number, msg):
+        pass
+
+    def warning_line(self, line_number, msg):
+        pass
+
+    def convention_line(self, line_number, msg):
+        pass
+
+    def info_line(self, line_number, msg):
+        pass
+
+    def refactor_line(self, line_number, msg):
+        pass
+
+    def fatal_line(self, line_number, msg):
+        pass
 
 
 class GeditEditor(Editor):
@@ -63,38 +80,62 @@ class GeditEditor(Editor):
         manager = GtkSource.LanguageManager()
         self.buff.set_language(manager.guess_language(self.filename, None))
 
+        self.view.connect('button-release-event', self.show_error_tag)
+
         # open file
         with open(self.filepath) as f:
             self.buff.set_text(f.read())
         self.view.show()
 
     def error_line(self, line_number, msg):
-        self.tag(line_number, msg, bc_color='red')
+        self._tag(line_number, msg, bc_color='red')
 
     def warning_line(self, line_number, msg):
-        self.tag(line_number, msg, bc_color='orange')
+        self._tag(line_number, msg, bc_color='orange')
 
     def convention_line(self, line_number, msg):
-        self.tag(line_number, msg, bc_color='orange')
+        self._tag(line_number, msg, bc_color='orange')
 
     def info_line(self, line_number, msg):
-        self.tag(line_number, msg, bc_color='orange')
+        self._tag(line_number, msg, bc_color='orange')
 
     def refactor_line(self, line_number, msg):
-        self.tag(line_number, msg, bc_color='orange')
+        self._tag(line_number, msg, bc_color='orange')
 
     def fatal_line(self, line_number, msg):
-        self.tag(line_number, msg, bc_color='red')
+        self._tag(line_number, msg, bc_color='red')
 
-    def tag(self, line, msg, bc_color='red'):
+    def _tag(self, line, msg, bc_color='red'):
 
         start = self.buff.get_iter_at_line(line)
         end = self.buff.get_iter_at_line(line)
         end.forward_line()
 
-        tag = Gtk.TextTag()
+        tag = self.ErrorTag(msg)
         tag.set_property('background', bc_color)
 
         self.buff.get_tag_table().add(tag)
         self.buff.apply_tag(tag, start, end)
+
+    def show_error_tag(self, widget, event):
+        '''
+        This function is invoked on text view click
+        Displays error if clicked on error tag
+        Author: Jan Vorcak <vorcak@mail.muni.cz>
+        @param - widget
+        @param - event
+        '''
+        offset = self.buff.get_property('cursor-position')
+        it = self.buff.get_iter_at_offset(offset)
+        for x in it.get_tags():
+            if hasattr(x, 'msg'):
+                print x.msg
+
+    class ErrorTag(Gtk.TextTag):
+
+        def __init__(self, msg):
+            Gtk.TextTag.__init__(self)
+            self.msg = msg
+
+
 
